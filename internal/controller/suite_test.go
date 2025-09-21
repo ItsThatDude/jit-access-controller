d076e1411347b79638a272ef830630e3991e6753
ItsThatDude <81509659+ItsThatDude@users.noreply.github.com>
2025-09-21 13:15:36 +1200

    move common funcs to suite_test

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -21,10 +21,13 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	assert "github.com/onsi/gomega"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -139,4 +142,27 @@ func getFirstFoundEnvTestBinaryDir() string {
 		}
 	}
 	return ""
+}
+
+func waitForCreated(ctx context.Context, c client.Client, key client.ObjectKey, obj client.Object) {
+	assert.Eventually(func() error { return c.Get(ctx, key, obj) }, 5*time.Second, 100*time.Millisecond).Should(assert.Succeed())
+}
+
+func waitForMarkedForDeletion(ctx context.Context, c client.Client, key client.ObjectKey, obj client.Object) {
+	assert.Eventually(func() bool {
+		err := c.Get(ctx, key, obj)
+		return err == nil && obj.GetDeletionTimestamp() != nil
+	}, 5*time.Second, 100*time.Millisecond).Should(assert.BeTrue())
+}
+
+func waitForDeleted(ctx context.Context, c client.Client, key client.ObjectKey, obj client.Object) {
+	assert.Eventually(func() bool { return errors.IsNotFound(c.Get(ctx, key, obj)) }, 5*time.Second, 100*time.Millisecond).Should(assert.BeTrue())
+}
+
+func reconcileOnce(ctx context.Context, r *GenericJITAccessReconciler, key client.ObjectKey) {
+	req := ctrl.Request{NamespacedName: key}
+	assert.Eventually(func() error {
+		_, err := r.Reconcile(ctx, req)
+		return err
+	}, 5*time.Second, 100*time.Millisecond).Should(assert.Succeed())
 }
