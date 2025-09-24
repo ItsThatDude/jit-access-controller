91e81499fe8371a64f0ae18a634461d0dfcbd8b8
ItsThatDude <81509659+ItsThatDude@users.noreply.github.com>
2025-09-24 17:23:49 +1200

    update log var

--- internal/controller/generic_controller.go
+++ internal/controller/generic_controller.go
@@ -348,7 +348,7 @@ func (r *GenericJITAccessReconciler) handleApproved(
 		log.Info("Created Adhoc Role for request", "name", obj.GetName(), "subject", spec.Subject, "role", adhocName)
 
 		if err := r.createRoleBinding(ctx, obj, adhocName, adhocName, isClusterScoped, isClusterRole); err != nil && !errors.IsAlreadyExists(err) {
-			log.Error(err, "an error occurred creating the adhoc role binding for the request", "name", obj.GetName(), "subject", spec.Subject, "roleKind", roleKind, "role", spec.Role)
+			log.Error(err, "an error occurred creating the adhoc role binding for the request", "name", obj.GetName(), "subject", spec.Subject, "role", adhocName)
 			return ctrl.Result{}, err
 		}
 		status.AdhocRoleBindingCreated = true
